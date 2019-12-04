35e156453b90b0f9a6e328c4440f126ac696314e
RohitM2019 <rohitmittal@sfhs.com>
2019-12-04 01:39:22 -0800

    Kludge together the plugin so it kind of sort of works on 1.14.4

--- src/io/chazza/pixelpresents/util/ColorUtil.java
+++ src/io/chazza/pixelpresents/util/ColorUtil.java
@@ -6,14 +6,7 @@ import java.util.stream.Collectors;
 
 public class ColorUtil {
     private static String translateAlternateColorCodes(String textToTranslate) {
-        char[] b = textToTranslate.toCharArray();
-        for (int i = 0; i < b.length - 1; i++) {
-            if (b[i] == '&' && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i+1]) > -1) {
-                b[i] = '§';
-                b[i+1] = Character.toLowerCase(b[i+1]);
-            }
-        }
-        return new String(b);
+        return textToTranslate;
     }
 
     public static String translate(String string) {
